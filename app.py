1ad81e6eb59d68750f0f860973f37b9b1abe292b
Roy Wang <royyi2012@gmail.com>
2022-10-27 09:45:32 -0700

    finish first version of front end

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from http import HTTPStatus
 from matcher import main
 from fastapi.templating import Jinja2Templates
@@ -21,6 +21,7 @@ def _initialize():
     matcher = main.Matcher()
     print('Initialized!')
 
+@app.post("/")
 @app.get("/", tags=["General"])
 def _index(request: Request):
     """Health check."""
@@ -39,20 +40,38 @@ def _index(request: Request):
          "track_name": list(matcher.df_tracks.loc[seed_list,'track_name'].values)})
 
 @app.post("/recommend", tags=["recommendation"])
-def _recommend(request: Request, seed_index):
+def _recommend(request: Request,
+               artist_input: str = Form(),
+               track_input: str = Form()):
     """Recommend the matched items given seed item"""
+    print(artist_input, track_input)
+    seed_index = matcher.df_tracks.loc[
+        (matcher.df_tracks['track_name']==track_input)&\
+        (matcher.df_tracks['artist_name']==artist_input) 
+    ].index.values[0]
+    seed_uri = str(matcher.df_tracks.loc[[seed_index],'track_id'].values[0])
+#    seed_uri = 'spotify:track:' + seed_uri
+    print(seed_index, seed_uri)
     recs = matcher.predict_tracks(seed_index)
-    #recs = pd.Series(
-    #    df_tracks.track_name.values, 
-    #    index=df_tracks.track_id).to_dict()
-    tracks = list(recs['track_name'].values)
-    artists = list(recs['artist_name'].values)
-    albums = list(recs['album_name'].values)
-    response = {
-        "message": HTTPStatus.OK.phrase,
-        "status-code":HTTPStatus.OK,
-        "data":{"tracks":tracks,
-                "artists":artists,
-                "alumbs":albums}
-    }
-    return response
+    posts = recs.to_dict(orient = 'records')
+#    tracks = list(recs['track_name'].values)
+#    artists = list(recs['artist_name'].values)
+#    albums = list(recs['album_name'].values)
+#    response = {
+#        "message": HTTPStatus.OK.phrase,
+#        "status-code":HTTPStatus.OK,
+#        "data":{"tracks":tracks,
+#                "artists":artists,
+#                "alumbs":albums}
+#    }
+#    return response
+    print(posts)
+    return templates.TemplateResponse(
+        "recommend.html",
+        {"request": request,
+         "posts":posts,
+         "artist_input":artist_input,
+         "track_input":track_input,
+         "track_uri":seed_uri,
+         "liked":""}
+    )
